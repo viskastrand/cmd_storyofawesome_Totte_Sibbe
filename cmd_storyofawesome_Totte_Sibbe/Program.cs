71c0ac368db1d102fcbb1c171405ad63c619a382
Totte Karlsson <totte.h@live.se>
2016-04-06 09:15:18 +0200

    first comit bla bladfsdf

---
+++ cmd_storyofawesome_Totte_Sibbe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmd_storyofawesome_Totte_Sibbe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
