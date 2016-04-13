f750708ed8e9cc8ef4af214348cc237e117a8ad0
Håkan Åberg <sibbe333@hotmail.se>
2016-04-13 08:43:57 +0200

    Ändringar

--- cmd_storyofawesome_Totte_Sibbe/Program.cs
+++ cmd_storyofawesome_Totte_Sibbe/Program.cs
@@ -10,7 +10,39 @@ namespace cmd_storyofawesome_Totte_Sibbe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("vill inte");
+            Boolean harNyckel = false;
+            string input;
+
+            Console.WriteLine("---Välkommen till Story Of Awesome---");
+            Console.WriteLine("\nDu är påväg hem och du ska gå in i ditt hus. Sedan göra dig i ordning för att sova.");
+
+            while (true) //oändlig loop
+            {
+                Console.Write("\n>");
+                input = Console.ReadLine();
+
+                if (input == "dörr" || input == "öppna dörr" || input == "gå till dörr")
+                {
+                    if (harNyckel)
+                    {
+                        Console.WriteLine("Du öppnar dörren och går igenom");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Den är låst");
+                }
+
+                else if (input == "nyckel" || input == "ta nyckel")
+                {
+                    Console.WriteLine("Du tog nyckeln");
+                    harNyckel = true;
+                }
+                else
+                    Console.WriteLine("Icke gilltigt eller felstavat commando!");
+
+            }
+
+            Console.WriteLine("\nDu vann!!! Spelet stängs av...");
             Console.ReadLine();
         }
     }
